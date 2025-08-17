44de700a5f66a1665fea7c2bc91979706a1ce947
CMPNION <norkindima57@gmail.com>
2025-08-17 18:25:23 +0500

    dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,20 @@ import { DatabaseModule } from "./modules/database/database.module";
 import { LocalesModule } from "./modules/locales/locales.module";
 import { CacheModule } from "./modules/cache/cache.module";
 import { EventsModule } from "./modules/events/events.module";
-import { AdminModule } from './modules/admin/admin.module';
+import { AdminModule } from "./modules/admin/admin.module";
+import { QueueModule } from './modules/queue/queue.module';
 
 @Module({
-  imports: [
-    ConfigHostModule,
-    DatabaseModule,
-    LocalesModule,
-    CacheModule,
-    EventsModule,
-    AdminModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigHostModule,
+        DatabaseModule,
+        LocalesModule,
+        CacheModule,
+        EventsModule,
+        AdminModule,
+        QueueModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,43 +3,43 @@ import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle("Документация проекта")
-    .setDescription("")
-    .setVersion("1.0")
-    .addBearerAuth(
-      {
-        type: "http",
-        scheme: "bearer",
-        bearerFormat: "JWT",
-        in: "header",
-        "x-tokenName": "Authorization",
-      },
-      "Authorization"
-    )
-    .build();
+    const config = new DocumentBuilder()
+        .setTitle("Документация проекта")
+        .setDescription("")
+        .setVersion("1.0")
+        .addBearerAuth(
+            {
+                type: "http",
+                scheme: "bearer",
+                bearerFormat: "JWT",
+                in: "header",
+                "x-tokenName": "Authorization",
+            },
+            "Authorization",
+        )
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+    const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup("/api/docs", app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
+    SwaggerModule.setup("/api/docs", app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+        },
+    });
 
-  app.enableCors();
+    app.enableCors();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // удаляет лишние поля, которых нет в DTO
-      forbidNonWhitelisted: true, // выбросит ошибку, если пришли лишние поля
-      transform: true, // преобразует строки в числа/даты, если указано в DTO
-    })
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true, // удаляет лишние поля, которых нет в DTO
+            forbidNonWhitelisted: true, // выбросит ошибку, если пришли лишние поля
+            transform: true, // преобразует строки в числа/даты, если указано в DTO
+        }),
+    );
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
 
 void bootstrap();
