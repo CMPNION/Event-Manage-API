0557538aa65cf7695aae4314ffd38b74074e810f
CMPNION <norkindima57@gmail.com>
2025-08-17 18:27:03 +0500

    queue module

---
+++ src/modules/queue/queue.module.ts
@@ -0,0 +1,15 @@
+import { BullModule } from "@nestjs/bullmq";
+import { Module } from "@nestjs/common";
+
+@Module({
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      },
+    }),
+    BullModule.registerQueue({ name: "event-notice" }),
+  ],
+})
+export class QueueModule {}
